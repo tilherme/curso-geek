e68311231dc64aaa843ec9eee2d905b011539037
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2023-03-12 17:26:41 -0300

    feat: method get

---
+++ cursos/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import Curso, Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        #extra_kwargs só vai mostrar no cadastro
+        model = Avaliacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'atualizacao',
+            'ativo',
+        )
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'atualizacao',
+            'ativo',
+        )
--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Avaliacao, Curso
+from .serializers import AvaliacaoSerializer, CursoSerializer
+
+class CursoAPIView(APIView):
+    """
+    Api curso
+    """
+    def get(self, request):
+        print(request.user)
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
