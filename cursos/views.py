acf2f748c8987b7a64bbbebc4649354ecfdfc4f1
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2023-03-14 20:36:33 -0300

    feat: method post

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from .models import Avaliacao, Curso
 from .serializers import AvaliacaoSerializer, CursoSerializer
 
@@ -14,9 +14,21 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
     
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'id': serializer.data['id'], 'curso':  serializer.data['titulo']}, status=status.HTTP_201_CREATED)
+    
 
 class AvaliacaoAPIView(APIView):
     def get(self, request):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer =  AvaliacaoSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
