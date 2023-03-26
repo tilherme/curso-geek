8e8b9142d1aedc0ff106a7986dbc4b7ecae69f05
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2023-03-26 00:32:32 -0300

    feat: overriding methods

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from .models import Avaliacao, Curso
 from .serializers import AvaliacaoSerializer, CursoSerializer
 
@@ -11,11 +13,22 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
-
 class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
     
+
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
